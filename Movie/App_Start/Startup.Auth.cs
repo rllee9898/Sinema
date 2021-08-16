a0c35bdfc699135c70591810e413ec93f438290f
Robert Lee <rllee9898@gmail.com>
2021-08-16 14:58:47 -0400

    Created Service Data Models and MVC Tier

--- Movie/App_Start/Startup.Auth.cs
+++ Movie/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using Movie.Models;
+using Movie.Data;
 
 namespace Movie
 {
--- Movie/Controllers/AccountController.cs
+++ Movie/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using Movie.Data;
 using Movie.Models;
 
 namespace Movie.Controllers
